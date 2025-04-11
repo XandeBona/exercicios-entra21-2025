2c956e3d8273f705fc467d6513dcecdf05737475
ALEXANDRE GABRIEL BONA <alexandre.bona3@alunos.sc.senac.br>
2025-04-11 20:37:58 -0300

    Realizado exercicio 11

---
+++ Exercicios/src/Exercicio11.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Exercicio11 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(10);
+        numeros.add(21);
+        numeros.add(42);
+        numeros.add(51);
+        numeros.add(17);
+        numeros.add(80);
+        numeros.add(200);
+        numeros.add(150);
+        numeros.add(80);
+
+        System.out.println(numeros);
+    }
+}
