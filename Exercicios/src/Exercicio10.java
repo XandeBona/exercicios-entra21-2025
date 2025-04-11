3eba9f83b1bf9552157a390c7f85b9994ce90114
ALEXANDRE GABRIEL BONA <alexandre.bona3@alunos.sc.senac.br>
2025-04-11 20:34:41 -0300

    Realizado exercicio 10

---
+++ Exercicios/src/Exercicio10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Informe um número para iniciar a contagem regressiva: ");
+        int numero = sc.nextInt();
+
+        for (int i = numero; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
